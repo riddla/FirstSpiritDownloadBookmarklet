7cf3c5e02f09796cf0779a64de19fa9ecdafd95d
Volker Rose <volker.rose@gmail.com>
2018-01-04 15:02:44 +0100

    Update FSChangeLauncherNames.js

--- FSChangeLauncherNames.js
+++ FSChangeLauncherNames.js
@@ -1,9 +1,7 @@
 javascript:(function() {
-// jquerify.js
-// https://github.com/bgrins/devtools-snippets
-// Add jQuery to any page that does not have it already.
-
-(function() {
+    // jquerify.js
+    // https://github.com/bgrins/devtools-snippets
+    // Add jQuery to any page that does not have it already.
 
     if (!window.jQuery) {
         var dollarInUse = !!window.$;
@@ -23,42 +21,40 @@ javascript:(function() {
         init();
     }
 
-}
-)();
+    function init() {
+        var licenseType = jQuery('.fs-license-type').text();
+        var licenseUser = jQuery('.fs-license-user').text();
 
-function init() {
-    var licenseType = jQuery('.fs-license-type').text();
-    var licenseUser = jQuery('.fs-license-user').text();
-
-    changeSiteArchitectLink(licenseType, licenseUser);
-    changeQuickStartLinks(licenseType, licenseUser);
-}
-
-function changeQuickStartLinks(licenseType, licenseUser) {
-    jQuery('.quickstart .projectlinks_entry').each(function() {
-        var $link = jQuery(this).find('a');
-        var text = $link.text().clean().replace(/\s/g, "_")
-        buildDownloadFileName($link, licenseType, licenseUser, text);
-    });
-}
-
-function changeSiteArchitectLink(licenseType, licenseUser) {
-    return buildDownloadFileName(jQuery('.button_sitearchitect'), licenseType, licenseUser);
-}
-
-function buildDownloadFileName($link, licenseType, licenseUser, additional) {
-    var downloadFileName = `${licenseUser}-${licenseType}`;
-
-    if (additional) {
-        downloadFileName += `_${additional}`;
+        changeSiteArchitectLink(licenseType, licenseUser);
+        changeQuickStartLinks(licenseType, licenseUser);
     }
 
-    downloadFileName += '.jnlp'
+    function changeQuickStartLinks(licenseType, licenseUser) {
+        jQuery('.quickstart .projectlinks_entry').each(function() {
+            var $link = jQuery(this).find('a');
+            var text = $link.text().clean().replace(/\s/g, "_")
+            buildDownloadFileName($link, licenseType, licenseUser, text);
+        });
+    }
 
-    console.log('changed', $link.attr('href'), 'to', downloadFileName);
+    function changeSiteArchitectLink(licenseType, licenseUser) {
+        return buildDownloadFileName(jQuery('.button_sitearchitect'), licenseType, licenseUser);
+    }
 
-    $link.attr('download', downloadFileName);
+    function buildDownloadFileName($link, licenseType, licenseUser, additional) {
+        var downloadFileName = `${licenseUser}-${licenseType}`;
 
-    return downloadFileName;
+        if (additional) {
+            downloadFileName += `_${additional}`;
+        }
+
+        downloadFileName += '.jnlp'
+
+        console.log('changed', $link.attr('href'), 'to', downloadFileName);
+
+        $link.attr('download', downloadFileName);
+
+        return downloadFileName;
+    }
 }
-})();
+)();
